4ec75dbfcca56a01bd05e3e4594ae2f26982477e
zilef2 <ajelof2@gmail.com>
2024-08-16 21:49:39 -0500

    ejemplo

---
+++ app/Http/Requests/FormularioTwoStoreRequest.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\Models\Permission;
+use Illuminate\Foundation\Http\FormRequest;
+
+class FormularioTwoStoreRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return [
+    //        'name' => 'required|string|max:255|unique:'.Permission::class,
+//            'numero_necesidad' => 'nullable',
+//            'identificacion_user' => 'required',
+            'proceso_que_solicita_presupuesto' => 'required',
+//            'valor_total_de_la_solicitud_actual' => 'required',
+//            'valor_total_asignado_en_vigencia_anterior' => 'nullable',
+
+//            todo 'nombre' => 'required',
+            'necesidad' => 'required',
+            'justificacion' => 'required',
+            'actividad' => 'required',
+            'categoria' => 'required',
+            'unidad_de_medida' => 'required',
+            'cantidad' => 'required|numeric|min:1|max:99999999999999',
+            'valor_unitario' => 'required',
+            'valor_total_solicitatdo_por_necesidad' => 'required',
+            'periodo_de_inicio_de_ejecucion' => 'required',
+            'vigencias_anteriores' => 'required',
+            'valor_asignado_en_la_vigencia_anterior' => 'required',
+
+//            'procesos_involucrados' => 'nullable',
+//            'plan_de_mejoramiento_al_que_apunta_la_necesidad' => 'required',
+//            'linea_del_plan_desarrollo_al_que_apunta_la_necesidad' => 'required',
+//
+//            'frecuencia_de_uso' => 'required',
+//            'mantenimientos_requeridos' => 'required',
+//            'capacidad_instalada' => 'required',
+//            'riesgo_de_la_inversion' => 'required',
+//            'anexos' => 'nullable|mimes:pdf,doc,docx,xls,xlsx'
+        ];
+    }
+}
